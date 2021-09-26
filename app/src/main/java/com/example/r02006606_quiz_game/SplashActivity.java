ff943f93c84568f59d0ee23acfba120ae9a88eb9
clofaso <christine.lofaso@gmail.com>
2021-09-26 00:52:24 -0400

    Added Launcher Theme to implement splash screen

---
+++ app/src/main/java/com/example/r02006606_quiz_game/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.r02006606_quiz_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
